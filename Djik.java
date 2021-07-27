cc6a460b641a5d1d136001296dce308cdd095a4a
umairmohd8 <mohammed08@protonmail.com>
2021-07-27 13:39:24 +0530

    Dijkstra updated

---
+++ Djik.java
@@ -0,0 +1,72 @@
+public class Djik {
+
+    void display(int cost[], int n){
+        System.out.println("The distance from source to index\nIndex\tDistance");
+        for(int i =0;i<n;i++){
+            System.out.println(i + "\t" + cost[i]);
+        }
+    }
+
+    int[] smallest(int cost[], int inc[],int n){
+        int low = Integer.MAX_VALUE;
+        int ind = -1;
+        int ret[] = new int[2];
+        for(int i =0; i<n;i++){
+            if(cost[i] < low && inc[i] == 0){
+                low = cost[i];
+                ind = i;
+            }
+        }
+        if(ind > -1){
+            inc[ind] = 1;
+        }
+        ret[0] = low;
+        ret[1] = ind;
+        return ret;
+    }
+
+    void path(int graph[][], int src){
+        int n = 9;
+        int inc[] = new int[n];
+        int cost[] = new int[n];
+
+        for(int i = 0; i< n;i++){
+            inc[i] = 0;
+            cost[i] = Integer.MAX_VALUE;
+        }
+
+        cost[src] = 0;
+
+        int u =-1;
+        do{
+            int[] values = smallest(cost, inc,n);
+            u = values[1];
+            int prev = values[0];
+
+            if(u > -1){
+                for(int v =0;v<n;v++){
+                    if(graph[u][v]> 0 && inc[v] ==0 && graph[u][v] + prev < cost[v]){
+                        cost[v] = graph[u][v]+ prev;
+                    }
+                }
+            }
+            else{
+                display(cost,n);
+            }
+            
+        }while(u > -1);
+    }
+    public static void main(String[] args) {
+        int graph[][] = new int[][] { { 0, 4, 0, 0, 0, 0, 0, 8, 0 }, 
+                { 4, 0, 8, 0, 0, 0, 0, 11, 0 },
+                { 0, 8, 0, 7, 0, 4, 0, 0, 2 }, 
+                { 0, 0, 7, 0, 9, 14, 0, 0, 0 }, 
+                { 0, 0, 0, 9, 0, 10, 0, 0, 0 },
+                { 0, 0, 4, 14, 10, 0, 2, 0, 0 }, 
+                { 0, 0, 0, 0, 0, 2, 0, 1, 6 }, 
+                { 8, 11, 0, 0, 0, 0, 1, 0, 7 },
+                { 0, 0, 2, 0, 0, 0, 6, 7, 0 } };
+        Djik ob1 = new Djik();
+        ob1.path(graph,4);
+    }
+}
--- Lab1a.java
+++ Lab1a.java
@@ -1,48 +1,52 @@
 import java.util.Scanner;
 
-class Student{
-    String usn,name,programme,phone;
-    // constructor    
-    public Student(String usn,String name,String programme, String phone){
-        this.usn =usn;
-        this.name =name;
-        this.programme =programme;
-        this.phone =phone;
-        }
-    void printInfo(){
+class Student {
+    String usn, name, programme, phone;
+
+    // constructor
+    public Student(String usn, String name, String programme, String phone) {
+        this.usn = usn;
+        this.name = name;
+        this.programme = programme;
+        this.phone = phone;
+    }
+
+    void read() {
+
+        Scanner sc = new Scanner(System.in);
+
+        // input details
+        System.out.println("Enter the usn,name,programme,phone of the student ");
+        usn = sc.nextLine();
+        name = sc.nextLine();
+        programme = sc.nextLine();
+        phone = sc.nextLine();
+    }
+
+    void printInfo() {
         System.out.println("The details of the student are:");
-        System.out.printf("USN:%s\nName:%s\nProgamme:%s\nPhone:%s",
-                            usn,name,programme,phone);
+        System.out.printf("USN:%s\nName:%s\nProgamme:%s\nPhone:%s", usn, name, programme, phone);
     }
 }
 
 public class Lab1a {
     public static void main(String[] args) {
         int n;
-        String usn,name,programme,phone;
+        //String usn, name, programme, phone;
 
-        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of variables:");
         n = Integer.parseInt(sc.nextLine());
         // array of objects
-        Student[] stud = new Student[n+1];
-        for(int i = 1; i<=n; i++){
-            // input details
-            System.out.println("Enter the usn,name,programme,phone of the student " + i);
-            usn = sc.nextLine();
-            name =sc.nextLine();
-            programme =sc.nextLine();
-            phone = sc.nextLine();
-            // object creation 
-            stud[i] = new Student(usn, name, programme, phone);
-        }
+        Student[] stud = new Student[n + 1];
+
         // printing student info
-        while(true){
+        while (true) {
             System.out.println("\nEnter the student number to print info or 0 to exit: ");
             int i = sc.nextInt();
-            if(i == 0) break;  
+            if (i == 0)
+                break;
             stud[i].printInfo();
-        }  
+        }
     }
 
 }
